df286b972c01d98aa644aabcc45d4f2cee1025ee
YagYz <83752022+YagYz@users.noreply.github.com>
2024-08-23 17:44:11 +0300

    Delete assets directory

--- assets/js/image-popup.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('.image-link').magnificPopup({type:'image'});
-});
-$('.test-popup-link').magnificPopup({
-    type: 'image'
-    // other options
-});
--- assets/js/img_zoom.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $(".zoom").hover(function(){
-        $(this).addClass('transition');
-    }, function(){
-        $(this).removeClass('transition');
-    });
-});
